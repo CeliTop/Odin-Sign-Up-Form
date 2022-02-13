e20abb068102f953d7532d19afe21caaf09d9d64
Celian Debethune <celian.debethune@gmail.com>
2022-02-13 14:13:27 +0100

    no JS

--- script.js
+++
@@ -1,7 +0,0 @@
-const inputs = document.querySelectorAll("input");
-
-function updateInput(e) {
-  console.log(this.value);
-}
-
-inputs.forEach((input) => input.addEventListener("focus", updateInput));
